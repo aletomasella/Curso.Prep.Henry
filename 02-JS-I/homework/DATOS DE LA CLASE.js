a64c8568b9160c1942dcae39a3576653bbbe4117
Alejandro Tomasella <aletomasella09@gmail.com>
2022-01-03 15:19:58 -0300

    homework nro2 finish

---
+++ 02-JS-I/homework/DATOS DE LA CLASE.js
@@ -0,0 +1,10 @@
+// Cualquier nro es true excepto el cero 0
+// cualquier string es true excepto un string vacio
+// AND && SOLO ES TRUE SI AMBOS SON TRUE
+// OR || SOLO ES FALSE SI AMBOS SON FALSE
+// NOT ! CAMBIA, SI ANTES ERA FALSE AHORA ES TRUE
+
+function Imprime (N) {for (i=0; i <=N; i++) { 
+    if (i <= 99)
+     { console.log(i)}; 
+    if ( i > 100) {return 'El ciclo se detuvo porque ingresaste demasiados numeros'}}};
